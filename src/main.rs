2e70bb262288f17921bb6f45084bc7b401a43a53
Vincent <vince.pno@gmail.com>
2025-05-10 11:37:15 +0200

    wasm setup

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,30 @@ mod utils;
 mod views;
 
 use std::sync::{Arc, Mutex};
-use axum::{Router, routing::get};
+use axum::{Router, http::header, routing::get, response::IntoResponse};
 use minijinja::Environment;
 use rusqlite::Connection;
-
 
 struct AppState {
     env: Environment<'static>,
     db: Arc<Mutex<Connection>>,
+}
+
+const WASM_JS: &[u8] = include_bytes!("../../rust_wasm/wasm_client/pkg/wasm_client.js");
+const WASM: &[u8] = include_bytes!("../../rust_wasm/wasm_client/pkg/wasm_client_bg.wasm");
+
+async fn serve_wasm_js() -> impl IntoResponse {
+    (
+        [(header::CONTENT_TYPE, "application/javascript")],
+        WASM_JS.to_vec(),
+    )
+}
+
+async fn serve_wasm() -> impl IntoResponse {
+    (
+        [(header::CONTENT_TYPE, "application/wasm")],
+        WASM.to_vec(),
+    )
 }
 
 #[tokio::main]
@@ -40,6 +56,8 @@ async fn main() {
     let app_state = Arc::new(AppState { env, db: conn });
     let app = Router::new()
         .route("/", get(controllers::home::controller_home))
+        .route("/pkg/wasm_client.js", get(serve_wasm_js))
+        .route("/pkg/wasm_client_bg.wasm", get(serve_wasm))
         .with_state(app_state);
     let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{port}"))
         .await
